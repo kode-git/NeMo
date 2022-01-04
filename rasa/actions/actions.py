7cdbcaa86cd46f31adc4fcf96c78f5f3b64cf005
giovanniPi997 <60937991+giovanniPi997@users.noreply.github.com>
2022-01-04 01:35:02 +0100

    Add Todo List [Add-Complete-Ask]

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -14,6 +14,8 @@ from rasa_sdk.events import SlotSet
 import rasa_sdk.events
 import smtplib
 import os
+from pymongo import MongoClient
+
 email=""
 mail=""
 city=""
@@ -60,20 +62,113 @@ class ActionShowWheater(Action):
         for e in entities:
             city=e['value']
               
- 
+        print(city)
         complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=dbd3b02d8958d62185d02e944cd5f522"
         api_link = requests.get(complete_api_link)
         api_data = api_link.json()
-        
-        if(api_data== '{"cod":"404","message":"city not found"}'):
-            return [SlotSet("city", 'NONE')]
-        else:
-            temp_city = ((api_data['main']['temp'])-273.15)
-            weather_desc = api_data['weather'][0]['description']
-            print(tracker.slots['city'])
-            dispatcher.utter_message(text=f"{'The weather in ' + city + ' is '+ weather_desc + ' and the temperature is '+ str(round(temp_city,1)) +'°'}")
-        
+        temp_city = ((api_data['main']['temp'])-273.15)
+        weather_desc = api_data['weather'][0]['description']
+        print(tracker.slots['city'])
+        dispatcher.utter_message(text=f"{'The weataomher in ' + city + ' is '+ weather_desc + ' and the temperature is '+ str(round(temp_city,1)) +'°'}")
+    
+        client = MongoClient('localhost:27017')
+        mydb= client['prova']
+        mycol= mydb['characters']
+        print(mycol)
+
+        data={'name':'oca toro'}
+        mycol.insert_one(data)
+
+        print(client.list_database_names())
+
+        print(mydb.list_collection_names())
+
+        for x in mycol.find({"name":"pino peppe"}) :
+            print(x) 
+
+        mycol.delete_one({"name":"pino peppe"})
+
         return []
+
+
+class ActionAddTodo(Action):
+
+    def name(self) -> Text:
+        return "action_add_todo"
+
+    def run(self, dispatcher,tracker,domain):
+        print("FROM ACTION_ADD_TODO" )
+         
+        entities = tracker.latest_message['entities']
+        for e in entities:
+            task=e['value']
+        print(task)
+        dispatcher.utter_message(text=f"{'I added ' + task + ' to your list'}")
+                
+        client = MongoClient('localhost:27017')
+        mydb= client['prova']
+        mycol= mydb['characters']
+        print(mycol)
+
+        data={'name':task}
+        mycol.insert_one(data)
+
+
+
+
+class ActionCompleteTodo(Action):
+
+    def name(self) -> Text:
+        return "action_complete_todo"
+
+    def run(self, dispatcher,tracker,domain):
+        print("FROM ACTION_COMPLETE_TODO" )
+         
+        entities = tracker.latest_message['entities']
+        for e in entities:
+            task=e['value']
+        print(task)
+        dispatcher.utter_message(text=f"{'You have completed your task: ' + task }")
+                
+        client = MongoClient('localhost:27017')
+        mydb= client['prova']
+        mycol= mydb['characters']
+        print(mycol)
+
+        data={'name':task}
+        mycol.delete_one(data)
+
+
+
+
+
+class ActionAskTodo(Action):
+
+    def name(self) -> Text:
+        return "action_ask_todo"
+
+    def run(self, dispatcher,tracker,domain):
+        print("FROM ACTION_ASK_TODO" )
+         
+        list=""
+        client = MongoClient('localhost:27017')
+        mydb= client['prova']
+        mycol= mydb['characters']
+        
+        num_task = mycol.find().count()
+        task = mycol.find()
+
+        i=0
+        for x in task:
+            i=i+1
+            
+            single_task = '\nNumber ' + str(i) + ': ' + x['name'] + '.'
+            list = list + single_task
+        
+        dispatcher.utter_message(text=f"{'You have ' + str(num_task) + ' in your TODO list: ' + list }")
+        
+                
+
 
 
 class ActionSendMail(Action):
