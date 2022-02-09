7d2d7f3b5405ac89a72471ab34053e36cb4e2bb9
kode-git <mariosessa64@gmail.com>
2022-02-09 12:55:33 -0800

    Cleaning project by useless files

--- nemo_app/urls.py
+++
@@ -1,7 +0,0 @@
-# from django.contrib import admin
-# from django.urls import path, include
-# from . import views
-# urlpatterns = [
-#     path('asr/', views.asr_transcribe),
-#     path('tts/', views.tts_translate)
-# ]
